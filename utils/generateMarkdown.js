75c96aea5dedd0084b2bb876e13e176002bcf99e
Reggiejr44 <115569377+Reggiejr44@users.noreply.github.com>
2023-02-02 15:05:44 -0500

    completed markdown.js that holds functions for license badge, link, and to generate markdown for readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,80 @@
+const licenseArr = ["Academic Free License v3.0", "Apache license 2.0", "Artistic license 2.0", "Boost Software License 1.0", "MIT"]
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license===licenseArr[0]){
+    return "[![License: AFL-3.0](https://img.shields.io/badge/License-AFL--3.0-lightgrey.svg)](https://opensource.org/licenses/AFL-3.0)"
+  } else if (license===licenseArr[1]){
+    return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)]"
+  } else if (license===licenseArr[2]){
+    return "[![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic%202.0-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)"
+  } else if (license===licenseArr[3]){
+    return "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)"
+  } else if (license===licenseArr[4]){
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+  } else {
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseLink(license) {
+  if (license===licenseArr[0]){
+    return `[${licenseArr[0]}](https://opensource.org/licenses/AFL-3.0)`
+  } else if (license===licenseArr[1]){
+    return `[${licenseArr[1]}](https://opensource.org/licenses/Apache-2.0)`
+  } else if (license===licenseArr[2]){
+    return `[${licenseArr[2]}](https://opensource.org/licenses/Artistic-2.0)`
+  } else if (license===licenseArr[3]){
+    return `[${licenseArr[3]}](https://www.boost.org/LICENSE_1_0.txt)`
+  } else if (license===licenseArr[4]){
+    return `[${licenseArr[4]}](https://opensource.org/licenses/MIT)`
+  } else {
+    return ""
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ## License
+  ${renderLicenseLink(data.license)}
+
+
+  ## Badges
+  ${renderLicenseBadge(data.license)}
+
+  ## Table of Contents
+  * [License](#license)
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [How to Contribute](#how-to-contribute)
+  * [Tests](#tests)
+  * [Questions?](#questions)
+
+  ## Description
+  ${data.description}
+
+  ## Installation
+  ${data.install}
+
+  ## Usage
+  ${data.usage}
+
+  ## How to Contribute
+  [Contributor Covenant](https://www.contributor-covenant.org/)  
+  ${data.contributing}
+
+  ## Tests
+  ${data.testing}
+
+  ## Reach me here: 
+  [${data.username}](https://github.com/${data.username})  
+  ${data.email}`;
 }
 
 module.exports = generateMarkdown;
